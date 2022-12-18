65f8c0098cf3f7912740dc8aad93debe0483ba51
Daniel Hwang <humble92@gmail.com>
2022-12-17 20:02:59 -0500

    MOD:draw menu

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -11,19 +11,22 @@
     ]
 </script>
 
-<nav>
-    <ul>
+<div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <!-- Page content here -->
+      <label for="my-drawer" class="btn btn-primary drawer-button">Open menu</label>
+    </div> 
+    <div class="drawer-side">
+      <label for="my-drawer" class="drawer-overlay"></label>
+      <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+        <!-- Sidebar content here -->
         {#each nav as item}
             <li>
                 <a class:active={$page.url.pathname === item.path} href={item.path}>{item.title}</a>
             </li>
         {/each}
-    </ul>
-</nav>
+      </ul>
+    </div>
+  </div>
 
-<style>
-    ul {display: flex; list-style: none;}
-    li {margin-right: 1em; }
-    li a {text-decoration: none;}
-    li:hover { text-decoration: underline;}
-</style>
